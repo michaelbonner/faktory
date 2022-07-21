edecdbe68c75b0acc0adbfeea68bfec2607e1a7e
Colton Bonner <cbonner26118@gmail.com>
2022-07-21 16:01:37 -0600

    Lowered vertical spacing on textSection and imageSection components.

--- web/components/sections/TextSection.js
+++ web/components/sections/TextSection.js
@@ -34,7 +34,7 @@ function TextSection({
         // color: `${textColor}`,
       }}
     >
-      <div className="max-w-7xl mx-auto px-4 py-12 lg:py-36 grid gap-8 lg:gap-16 items-center user-content">
+      <div className="max-w-7xl mx-auto px-4 py-12 lg:py-24 grid gap-8 items-center user-content">
         <h2>{heading}</h2>
         {text && <SimpleBlockContent blocks={text} />}
         {cta && cta.route && <Cta {...cta} />}
