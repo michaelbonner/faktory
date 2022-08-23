1cb8f8805a5c2b571753695e0cfc51bc77ad98f2
Michael Bonner <michaelwbonner@gmail.com>
2022-08-22 18:41:57 -0600

    add a few redirects to test

--- web/next.config.js
+++ web/next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/hire-us/",
+        destination: "/contact",
+        permanent: true,
+      },
+      {
+        source: "/home-2/",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/industry/",
+        destination: "/work",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
