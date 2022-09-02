bdc17e1fce8633614706d09008e9fb70b91774e5
Michael Bonner <michaelwbonner@gmail.com>
2022-09-01 19:57:53 -0600

    update theme color

--- web/components/Layout.js
+++ web/components/Layout.js
@@ -44,7 +44,7 @@ function Layout(props) {
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="msapplication-TileColor" content="#da532c" />
-        <meta name="theme-color" content="#ffffff" />
+        <meta name="theme-color" content="#746f4b" />
         <meta
           name="viewport"
           content="initial-scale=1.0, width=device-width, viewport-fit=cover"
