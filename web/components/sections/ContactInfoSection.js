c079bf5b90f73cd4bc670e34a8a76b68b36cf6f5
Michael Bonner <michaelwbonner@gmail.com>
2023-01-16 12:36:32 -0700

    scroll to top of form container on contact submit

--- web/components/sections/ContactInfoSection.js
+++ web/components/sections/ContactInfoSection.js
@@ -30,6 +30,8 @@ const serializeForm = function (form) {
 function ContactInfoSection({ contact, emailTo }) {
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const formContainerRef = useRef(null);
   const formRef = useRef(null);
 
   return (
@@ -71,160 +73,169 @@ function ContactInfoSection({ contact, emailTo }) {
                 info@faktorymail.com
               </a>
             </div>
-            {isSubmitted && (
-              <div
-                className="my-12 grid items-stretch border-b border-gold"
-                style={{ height: `${formRef?.current?.offsetHeight}px` }}
-              >
-                <div>
-                  <h2 className="text-3xl font-bold text-gold">Thank you!</h2>
-                  <p className="mt-8 text-lg">
-                    We have received your message and will be in touch shortly.
-                  </p>
+            <div ref={formContainerRef}>
+              {isSubmitted && (
+                <div
+                  className="my-12 grid items-stretch border-b border-gold"
+                  style={{ height: `${formRef?.current?.offsetHeight}px` }}
+                >
+                  <div>
+                    <h2 className="text-3xl font-bold text-gold">Thank you!</h2>
+                    <p className="mt-8 text-lg">
+                      We have received your message and will be in touch
+                      shortly.
+                    </p>
+                  </div>
+                  <div className="min-h-[200px] flex items-center grow justify-end">
+                    {/* eslint-disable-next-line @next/next/no-img-element */}
+                    <img
+                      alt="Brand bars"
+                      height="30px"
+                      src="/images/brand-bars.svg"
+                      width="180px"
+                    />
+                  </div>
                 </div>
-                <div className="min-h-[200px] flex items-center grow justify-end">
-                  {/* eslint-disable-next-line @next/next/no-img-element */}
-                  <img
-                    alt="Brand bars"
-                    height="30px"
-                    src="/images/brand-bars.svg"
-                    width="180px"
-                  />
-                </div>
-              </div>
-            )}
-            {!isSubmitted && (
-              <form
-                className={classNames(
-                  "grid grid-cols-1 gap-y-8 gap-x-4 transition-opacity",
-                  "md:grid-cols-2",
-                  isSubmitting && "opacity-50"
-                )}
-                ref={formRef}
-                onSubmit={async (event) => {
-                  event.preventDefault();
+              )}
+              {!isSubmitted && (
+                <form
+                  className={classNames(
+                    "grid grid-cols-1 gap-y-8 gap-x-4 transition-opacity",
+                    "md:grid-cols-2",
+                    isSubmitting && "opacity-50"
+                  )}
+                  ref={formRef}
+                  onSubmit={async (event) => {
+                    event.preventDefault();
 
-                  setIsSubmitting(true);
+                    setIsSubmitting(true);
 
-                  try {
-                    const form = event.target;
-                    const data = serializeForm(form);
-                    data.emailTo = emailTo;
+                    try {
+                      const form = event.target;
+                      const data = serializeForm(form);
+                      data.emailTo = emailTo;
 
-                    const submitForm = await fetch("/api/contact", {
-                      method: "POST",
-                      headers: {
-                        "Content-Type": "application/json",
-                      },
-                      body: JSON.stringify(data),
-                    });
-                    const response = await submitForm.json();
+                      const submitForm = await fetch("/api/contact", {
+                        method: "POST",
+                        headers: {
+                          "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify(data),
+                      });
+                      const response = await submitForm.json();
 
-                    if (response.success) {
-                      setIsSubmitted(true);
-                    } else {
-                      alert(
-                        "There was an error submitting your form. Please try again."
-                      );
+                      if (response.success) {
+                        setIsSubmitted(true);
+                        formContainerRef.current.scrollIntoView({
+                          behavior: "smooth",
+                        });
+                      } else {
+                        alert(
+                          "There was an error submitting your form. Please try again."
+                        );
+                      }
+
+                      setIsSubmitting(false);
+                    } catch (error) {
+                      setIsSubmitting(false);
                     }
+                  }}
+                >
+                  <div>
+                    <label
+                      className={classList.fieldLabel}
+                      htmlFor="first_name"
+                    >
+                      First Name*
+                    </label>
+                    <input
+                      className={classList.textField}
+                      type="text"
+                      name="first_name"
+                      id="first_name"
+                      placeholder="First Name"
+                      required
+                    />
+                  </div>
+                  <div>
+                    <label className={classList.fieldLabel} htmlFor="last_name">
+                      Last Name*
+                    </label>
+                    <input
+                      className={classList.textField}
+                      type="text"
+                      name="last_name"
+                      id="last_name"
+                      placeholder="Last Name"
+                      required
+                    />
+                  </div>
+                  <div>
+                    <label className={classList.fieldLabel} htmlFor="phone">
+                      Phone*
+                    </label>
+                    <input
+                      className={classList.textField}
+                      type="text"
+                      name="phone"
+                      id="phone"
+                      placeholder="(000) 000-0000"
+                      required
+                    />
+                  </div>
+                  <div>
+                    <label className={classList.fieldLabel} htmlFor="email">
+                      Email*
+                    </label>
+                    <input
+                      className={classList.textField}
+                      type="email"
+                      name="email"
+                      id="email"
+                      placeholder="you@organization.com"
+                      required
+                    />
+                  </div>
+                  <div>
+                    <label className={classList.fieldLabel} htmlFor="job_title">
+                      Job Title
+                    </label>
+                    <input
+                      className={classList.textField}
+                      type="text"
+                      name="job_title"
+                      id="job_title"
+                      placeholder="Job Title"
+                    />
+                  </div>
+                  <div className="md:col-span-2">
+                    <label
+                      className={classList.fieldLabel}
+                      htmlFor="tell_us_more"
+                    >
+                      Tell Us More
+                    </label>
+                    <textarea
+                      rows="8"
+                      name="tell_us_more"
+                      id="tell_us_more"
+                      className={classList.textField}
+                      placeholder="What brand or marketing problems are you trying to solve?"
+                    ></textarea>
+                  </div>
 
-                    setIsSubmitting(false);
-                  } catch (error) {
-                    setIsSubmitting(false);
-                  }
-                }}
-              >
-                <div>
-                  <label className={classList.fieldLabel} htmlFor="first_name">
-                    First Name*
-                  </label>
-                  <input
-                    className={classList.textField}
-                    type="text"
-                    name="first_name"
-                    id="first_name"
-                    placeholder="First Name"
-                    required
-                  />
-                </div>
-                <div>
-                  <label className={classList.fieldLabel} htmlFor="last_name">
-                    Last Name*
-                  </label>
-                  <input
-                    className={classList.textField}
-                    type="text"
-                    name="last_name"
-                    id="last_name"
-                    placeholder="Last Name"
-                    required
-                  />
-                </div>
-                <div>
-                  <label className={classList.fieldLabel} htmlFor="phone">
-                    Phone*
-                  </label>
-                  <input
-                    className={classList.textField}
-                    type="text"
-                    name="phone"
-                    id="phone"
-                    placeholder="(000) 000-0000"
-                    required
-                  />
-                </div>
-                <div>
-                  <label className={classList.fieldLabel} htmlFor="email">
-                    Email*
-                  </label>
-                  <input
-                    className={classList.textField}
-                    type="email"
-                    name="email"
-                    id="email"
-                    placeholder="you@organization.com"
-                    required
-                  />
-                </div>
-                <div>
-                  <label className={classList.fieldLabel} htmlFor="job_title">
-                    Job Title
-                  </label>
-                  <input
-                    className={classList.textField}
-                    type="text"
-                    name="job_title"
-                    id="job_title"
-                    placeholder="Job Title"
-                  />
-                </div>
-                <div className="md:col-span-2">
-                  <label
-                    className={classList.fieldLabel}
-                    htmlFor="tell_us_more"
-                  >
-                    Tell Us More
-                  </label>
-                  <textarea
-                    rows="8"
-                    name="tell_us_more"
-                    id="tell_us_more"
-                    className={classList.textField}
-                    placeholder="What brand or marketing problems are you trying to solve?"
-                  ></textarea>
-                </div>
-
-                <div className="md:col-span-2 flex flex-wrap gap-4 justify-between items-end">
-                  <Turnstile
-                    options={{
-                      theme: "light",
-                    }}
-                    siteKey={process.env.NEXT_PUBLIC_TURNSTILE_SITE_KEY}
-                  />
-                  <button className="standardButton">Submit</button>
-                </div>
-              </form>
-            )}
+                  <div className="md:col-span-2 flex flex-wrap gap-4 justify-between items-end">
+                    <Turnstile
+                      options={{
+                        theme: "light",
+                      }}
+                      siteKey={process.env.NEXT_PUBLIC_TURNSTILE_SITE_KEY}
+                    />
+                    <button className="standardButton">Submit</button>
+                  </div>
+                </form>
+              )}
+            </div>
           </div>
         </div>
       </div>
