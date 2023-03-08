ad66ace97cfab342f072744422a851e1168f10e1
Colton Bonner <cbonner26118@gmail.com>
2023-03-07 20:01:43 -0700

    Added optional contact email template.

--- web/pages/api/contact.js
+++ web/pages/api/contact.js
@@ -24,6 +24,7 @@ export default async function handler(request, response) {
     first_name,
     job_title,
     last_name,
+    organization = "",
     phone,
     tell_us_more,
   } = request.body;
@@ -36,6 +37,7 @@ export default async function handler(request, response) {
     first_name,
     job_title,
     last_name,
+    organization,
     phone,
     tell_us_more,
     createdAt: Date.now(),
@@ -102,46 +104,91 @@ const sendEmailWithSes = async ({
   first_name,
   last_name,
   job_title,
+  organization,
   phone,
   tell_us_more,
 }) => {
-  try {
-    await sendSesEmail(
-      [
-        {
-          name: "Faktory Contact Form",
-          address: `noreply@faktoryagency.com`,
-        },
-      ],
-      [
-        {
-          name: "Faktory Contact",
-          address: emailTo,
-        },
-        {
-          name: "Michael Bonner",
-          address: "mike@bootpackdigital.com",
-        },
-      ],
-      `New Faktory contact submission from ${first_name} ${last_name}`,
-      `<p><strong>Name</strong>:<br />${first_name} ${last_name}</p>
-      <p><strong>Phone</strong>:<br />${phone}</p>
-      <p><strong>Email</strong>:<br />${email}</p>
-      <p><strong>Job Title</strong>:<br />${job_title}</p>
-      <p><strong>Tell Us More</strong>:<br /> ${tell_us_more}</p>`.replaceAll(
-        "\n",
-        "<br />"
-      ),
-      `Name:\n ${first_name} ${last_name}\n
-      Phone:\n ${phone}\n
-      Email:\n ${email}\n
-      Job Title:\n ${job_title}\n
-      Tell Us More:\n ${tell_us_more}`,
-      email
-    );
-    return { success: true };
-  } catch (error) {
-    console.error("ses error", error);
-    return { success: false };
+  if (organization === "") {
+    try {
+      await sendSesEmail(
+        [
+          {
+            name: "Faktory Contact Form",
+            address: `noreply@faktoryagency.com`,
+          },
+        ],
+        [
+          {
+            name: "Faktory Contact",
+            address: emailTo,
+          },
+          {
+            name: "Michael Bonner",
+            address: "mike@bootpackdigital.com",
+          },
+        ],
+        `New Faktory contact submission from ${first_name} ${last_name}`,
+        `<p><strong>Name</strong>:<br />${first_name} ${last_name}</p>
+        <p><strong>Phone</strong>:<br />${phone}</p>
+        <p><strong>Email</strong>:<br />${email}</p>
+        <p><strong>Job Title</strong>:<br />${job_title}</p>
+        <p><strong>Tell Us More</strong>:<br /> ${tell_us_more}</p>`.replaceAll(
+          "\n",
+          "<br />"
+        ),
+        `Name:\n ${first_name} ${last_name}\n
+          Phone:\n ${phone}\n
+          Email:\n ${email}\n
+          Job Title:\n ${job_title}\n
+          Tell Us More:\n ${tell_us_more}`,
+        email
+      );
+      return { success: true };
+    } catch (error) {
+      console.error("ses error", error);
+      return { success: false };
+    }
+  } else {
+    try {
+      await sendSesEmail(
+        [
+          {
+            name: "Faktory Contact Form",
+            address: `noreply@faktoryagency.com`,
+          },
+        ],
+        [
+          {
+            name: "Faktory Contact",
+            address: emailTo,
+          },
+          {
+            name: "Colton Bonner",
+            address: "colton@bootpackdigital.com",
+          },
+        ],
+        `New Faktory gated document submission from ${first_name} ${last_name}`,
+        `<p><strong>Name</strong>:<br />${first_name} ${last_name}</p>
+            <p><strong>Phone</strong>:<br />${phone}</p>
+            <p><strong>Email</strong>:<br />${email}</p>
+            <p><strong>Job Title</strong>:<br />${job_title}</p>
+            <p><strong>Organization</strong>:<br />${organization}</p>
+            <p><strong>Tell Us More</strong>:<br /> ${tell_us_more}</p>`.replaceAll(
+          "\n",
+          "<br />"
+        ),
+        `Name:\n ${first_name} ${last_name}\n
+              Phone:\n ${phone}\n
+              Email:\n ${email}\n
+              Job Title:\n ${job_title}\n
+              Organization:\n ${organization}\n
+              Tell Us More:\n ${tell_us_more}`,
+        email
+      );
+      return { success: true };
+    } catch (error) {
+      console.error("ses error", error);
+      return { success: false };
+    }
   }
 };
